38821eb9a24d2d8bc09bd17158839b25a2a5ceba
seris370 <1812563232@qq.com>
2021-11-07 15:06:27 +0800

    implement CRUD

---
+++ crud/src/main/java/com/task/crud/CrudApplication.java
@@ -0,0 +1,48 @@
+package com.task.crud;
+
+import com.task.crud.model.Item;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/items")
+    public String getText() {
+        return Item.getSerializedItems();
+    }
+
+    @PostMapping("/add")
+    public String addItem(@RequestParam(name="text") String text) {
+        int id = Item.addItem(text);
+        return "Item added: " + id;
+    }
+
+    @PutMapping("/replace")
+    public String replaceItem(@RequestParam(name="id") int id, @RequestParam(name="text") String text) {
+        boolean res = Item.replaceItem(id, text);
+        return res ? "Item replaced" : "Fail to replace item";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteEmployee(@RequestParam(name="id") int id) {
+        boolean res = Item.deleteItem(id);
+        return res ? "Item deleted" : "Fail to delete item";
+    }
+}
---
+++ crud/src/main/java/com/task/crud/model/Item.java
@@ -0,0 +1,78 @@
+package com.task.crud.model;
+
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Item {
+    private static volatile int count = 0;
+
+    private static volatile LinkedList<Item> items = new LinkedList<>();
+
+    private static final Object lock = new Object();
+
+    private int id;
+
+    private String text;
+
+    private Item(String text) {
+        this.id = count;
+        this.text = text;
+    }
+
+    private int getId() {
+        return this.id;
+    }
+
+    private String getText() {
+        return this.text;
+    }
+
+    private void changeText(String text) {
+        this.text = text;
+    }
+
+    private String getItem() {
+        return this.id + ": " + this.text;
+    }
+
+    public static int addItem(String text) {
+        synchronized (lock) {
+            Item item = new Item(text);
+            count += 1;
+            items.add(item);
+            return count - 1;
+        }
+    }
+
+    public static boolean replaceItem(int id, String text) {
+        synchronized (lock) {
+            for (Item item : items) {
+                if (item.getId() == id) {
+                    item.changeText(text);
+                    return true;
+                } else if (item.getId() > id) {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean deleteItem(int id) {
+        synchronized (lock) {
+            for (Item item : items) {
+                if (item.getId() == id) {
+                    items.remove(item);
+                    return true;
+                } else if (item.getId() > id) {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static String getSerializedItems() {
+        return items.stream().map(Item::getItem).collect(Collectors.joining("\n"));
+    }
+}
