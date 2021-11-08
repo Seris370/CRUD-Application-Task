fefe719235d9e35a58d029d0b19d0c5a388b0bb2
seris370 <1812563232@qq.com>
2021-11-09 01:39:45 +0800

    enable CORS

--- crud/src/main/java/com/task/crud/CrudApplication.java
+++ crud/src/main/java/com/task/crud/CrudApplication.java
@@ -3,6 +3,7 @@ package com.task.crud;
 import com.task.crud.model.Item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,28 +20,33 @@ public class CrudApplication {
     }
 
     @GetMapping("/hello")
+    @CrossOrigin(origins = "http://localhost:3000")
     public String sayHello() {
         return "Hello World!";
     }
 
     @GetMapping("/items")
+    @CrossOrigin(origins = "http://localhost:3000")
     public String getText() {
         return Item.getSerializedItems();
     }
 
     @PostMapping("/add")
+    @CrossOrigin(origins = "http://localhost:3000")
     public String addItem(@RequestParam(name="text") String text) {
         int id = Item.addItem(text);
         return "Item added: " + id;
     }
 
     @PutMapping("/replace")
+    @CrossOrigin(origins = "http://localhost:3000")
     public String replaceItem(@RequestParam(name="id") int id, @RequestParam(name="text") String text) {
         boolean res = Item.replaceItem(id, text);
         return res ? "Item replaced" : "Fail to replace item";
     }
 
     @DeleteMapping("/delete")
+    @CrossOrigin(origins = "http://localhost:3000")
     public String deleteEmployee(@RequestParam(name="id") int id) {
         boolean res = Item.deleteItem(id);
         return res ? "Item deleted" : "Fail to delete item";
